0a7b4436429afb1b9990296dbaf26dc90e9a8f3d
= <neymegen50@gmail.com>
2022-09-21 15:06:02 +0300

    message2

--- src/UsersList.jsx
+++ src/UsersList.jsx
@@ -9,12 +9,15 @@ class UsersList extends Component {
     super(props);
     this.state = {
       filterText: '',
-      users: this.props.users,
+      users: this.props.users.slice(),
+      count: 0,
     };
   }
   onChange = (event) => {
     this.setState({
-      filterText: event.target.value
+      filterText: event.target.value,
+      users: this.props.users.filter(user=> user.name.toLowerCase().includes(text.target.value.toLowerCase())),
+      count: this.state.users.length,
     });
   }
     render() {
